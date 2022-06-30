7c43cea37732972f754a5037191231aae2085bf4
taisbelucik <taisbelucik@hotmail.com>
2022-06-30 16:48:16 -0300

    Menu dropdown

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
+
 
 
 initScrollSuave();
@@ -12,3 +14,4 @@ initAccordion();
 initTabNav();
 initModal();
 initTooltip();
+initDropdownMenu();
